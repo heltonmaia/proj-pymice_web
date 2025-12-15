873029c188e0bc4217b411055210f90370d2983c
Helton Maia <helton.maia@gmail.com>
2025-12-15 11:11:41 -0300

    remove panel version

--- pymice-panel/src/pymicetracking_panel/irl_tab/irl_tab.py
+++
@@ -1,8 +0,0 @@
-import panel as pn
-
-
-def get_tab() -> pn.Column:
-    return pn.Column(
-        pn.pane.Markdown("## IRL Analysis\nIRL analysis tools will appear here."),
-        margin=(10, 0),
-    )
